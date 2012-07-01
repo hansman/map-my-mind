ce95ff5d7fd93556cc4599c0a1a12ddf3d6c8369
hansman <steinbrecher.johann@gmail.com>
2012-07-01 14:44:17 -0700

    I update the datalist now using AJAX, i.e. JS JSON PHP MYSQL

---
+++ php/gettotalpapers.php
@@ -0,0 +1,35 @@
+<?php 
+  
+include 'config.php';
+
+$conn = mysql_connect("$dbhost", "$dbuser", "$dbpass");
+
+if (!$conn)
+{
+	print "test";
+	die('Could not connect: ' . mysql_error());
+}
+
+mysql_select_db("DM");
+$query  = "select doi, author, title, date, publisher from lit_testuser";
+$result=mysql_query($query);
+if (!$result) 
+{ 
+	echo "failed";	
+	die ("Query Failed.");
+}
+
+   
+   $jsonrows = array();
+   while($row = mysql_fetch_assoc($result)) {
+   	$jsonrows[] = $row;
+   }
+   
+   
+   echo json_encode($jsonrows);
+   
+   
+mysql_close($conn);
+
+
+?>
