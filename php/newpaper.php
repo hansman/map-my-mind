a8c31d7e228b0c209de92fe27b511f399c38c626
hansman <steinbrecher.johann@gmail.com>
2012-07-01 22:54:00 -0700

    I am now able to remove papers from the datalist ...

--- php/newpaper.php
+++ php/newpaper.php
@@ -1,8 +1,7 @@
 <?php 
 
 
-  $doi = $_GET["doi"];
-  
+  $doi = $_GET["doi"];  
   $date=$_GET["date"];
   $publisher=$_GET["publisher"];
   $author=$_GET["author"];
@@ -11,8 +10,7 @@
   include 'config.php';
   
   
-  $conn = mysql_connect("$dbhost", "$dbuser", "$dbpass");
-  
+  $conn = mysql_connect("$dbhost", "$dbuser", "$dbpass");  
   if (!$conn)
   {
   	print "test";
@@ -20,18 +18,13 @@
   }
   
    mysql_select_db("DM");
-   $query  = "insert into gcclab.regressionmail (name) value ('". $address ."')";
    $query  = "insert into lit_testuser(doi, author, title, date, publisher) values ('". $doi ."','". $author ."','". $title ."','". $date ."','". $publisher ."' );";
-   print "$query";
-  //$query  = "insert into lit_testuser(doi) values ( $doi);";
+   //print "$query";
+ 
    $result=mysql_query($query);
    if (!$result) die ("Query Failed.");
-   //$result = $conn->get_results($query);
-  //if ($result) die ("Submit Failed.");
   
-  mysql_close($conn);
-  
-   
+   mysql_close($conn); 
 
 ?>
 
---
+++ php/removepaper.php
@@ -0,0 +1,24 @@
+<?php 
+
+  $title = $_GET["title"];
+  include 'config.php';
+  
+  $conn = mysql_connect("$dbhost", "$dbuser", "$dbpass");
+  if (!$conn)
+  {
+  	print "test";
+  	die('Could not connect: ' . mysql_error());
+  }
+  
+   mysql_select_db("DM");
+   $query  = "delete from lit_testuser where title=' ". $title ."'";
+   
+   echo "$query";
+   
+   $result=mysql_query($query);
+   if (!$result) die ("Query Failed.");
+  
+   mysql_close($conn);   
+
+?>
+
