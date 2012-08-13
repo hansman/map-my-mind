d717b51cd65a386da5a124ff04926f69eebbbbf3
hansman <steinbrecher.johann@gmail.com>
2012-08-12 23:50:05 -0700

    Use mysql_real_escape_string to prevent from mysql injection

--- php/login.php
+++ php/login.php
@@ -1,55 +1,47 @@
 <?php 
   
-include 'config.php';
+	include 'config.php';
 
-$username = $_GET["name"];
-$password = $_GET["pass"];
+	$username = $_GET["name"];
+	$password = $_GET["pass"];
 
-$conn = mysql_connect("$dbhost", "$dbuser", "$dbpass");
+	$conn = mysql_connect("$dbhost", "$dbuser", "$dbpass");
 
-if (!$conn)
-{
-	die('Could not connect: ' . mysql_error());
-}
+	if (!$conn)
+	{
+		die('Could not connect: ' . mysql_error());
+	}
 
-mysql_select_db("DM");
+	mysql_select_db("DM");
 
-$query = "lock table accounts read";
-mysql_query($query);
+	$query = "lock table accounts read";
+	mysql_query($query);
 
-$query = "select id from accounts where username='". $username ."' and pswd=MD5('". $password ."')";
+	$query = "select id from accounts where username='". $username ."' and pswd=MD5('". mysql_real_escape_string($password) ."')";
 	
-$result=mysql_query($query);
+	$result=mysql_query( $query );
 
-$query = "unlock table";
-mysql_query($query);
+	$query = "unlock table";
+	mysql_query($query);
 
-if (!$result) 
-{   
-	die ("Query Failed.");
-}
-else if (mysql_num_rows($result) == 0  )
-{
-	
-	echo "failed";
-	
-}
-else    
-{
-   session_start();
-   if(!isset($_SESSION['activeuser']))
-   {
-	$_SESSION['activeuser'] = $username;
-	$_SESSION['activeID'] = mysql_result($result, 0, 'id');;
-	//print("--". $_SESSION['activeuser'] . "--" . $_SESSION['activeID'] ."--");
-	
-   }
+	if (!$result) 
+	{   
+		die ("Query Failed.");
+	}
+	else if (mysql_num_rows($result) == 0  )
+	{	
+		echo "failed";
+	}
+	else    
+	{
+   		session_start();
+   		if(!isset($_SESSION['activeuser']))
+   		{
+			$_SESSION['activeuser'] = $username;
+			$_SESSION['activeID'] = mysql_result($result, 0, 'id');
+   		}
    	
-}
+	}
 
-
-   
-mysql_close($conn);
-
-
+	mysql_close($conn);
 ?>
