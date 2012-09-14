e09faddb5ebbcfcfd73c29cd89fe55d7db199da5
hansman <steinbrecher.johann@gmail.com>
2012-09-14 00:19:08 -0700

    Missed the Logout class

---
+++ php/engines/Logout.php
@@ -0,0 +1,18 @@
+<?php 
+
+class Logout
+{
+	
+	public function run()
+	{
+				
+		session_start();
+			if (isset($_SESSION['activeuser']))
+				unset($_SESSION['activeuser']);
+
+			if (isset($_SESSION['activeID']))
+				unset($_SESSION['activeID']);
+	}	
+}
+
+?>
